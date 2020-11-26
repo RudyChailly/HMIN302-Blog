6d2a256abd28b0ef99e2fead73ea38cd7b8ba473
rudychailly <rudy.chailly@gmail.com>
2020-11-26 15:46:44 +0100

    login + user crud

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/")
+ * @Route("/articles")
  */
 class ArticleController extends AbstractController
 {
---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use phpDocumentor\Reflection\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes ne sont pas identiques.',
+                'first_options' => ['label' => 'password'],
+                'second_options' => ['label' => 'repeat password']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
